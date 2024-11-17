5e6c47d57e97ea241774173e96ced6278bc074da
antoinezephy312 <antoinezephyr312@gmail.com>
2024-11-17 20:49:21 +0800

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage'); // Import sendMessage correctly
 
 module.exports = {
   name: 'feedback',
@@ -6,7 +7,7 @@ module.exports = {
   usage: 'feedback <your message>',
   author: 'Clarence',
   
-  async execute(senderId, args, pageAccessToken, sendMessage) {
+  async execute(senderId, args, pageAccessToken) {
     // Check if there's a message provided
     if (!args || args.length === 0) {
       await sendMessage(senderId, { text: 'Enter your message first!\nUsage: feedback <your message>' }, pageAccessToken);
@@ -17,21 +18,21 @@ module.exports = {
     const feedbackMessage = args.join(' ');
 
     try {
-      // Admin Facebook Messenger thread URL
+      // Admin Messenger endpoint
       const adminMessengerUrl = 'https://www.facebook.com/messages/t/100029573642160';
 
-      // Send feedback to admin
+      // Send feedback message (simulate sending to admin URL)
       await axios.post(adminMessengerUrl, {
         message: `Feedback from user ${senderId}:\n\n${feedbackMessage}`
       });
 
-      // Respond to the user if feedback was sent successfully
+      // Confirm feedback sent to the user
       await sendMessage(senderId, {
-        text: 'Thanks for your feedback! Your feedbacks are really helpful to improve our service. Thank you and enjoy using!'
+        text: 'Thanks for your feedback! Your feedback is really helpful to improve our service. Thank you and enjoy using!'
       }, pageAccessToken);
     } catch (error) {
       console.error('Error sending feedback:', error);
-      // Respond to the user in case of an error
+      // Respond to the user if there was an error
       await sendMessage(senderId, { text: 'Error: Could not send your feedback.' }, pageAccessToken);
     }
   }
