8555fde22b1672faed1a4f3c0fb9fd5b566018bd
antoinezephy312 <antoinezephyr312@gmail.com>
2024-12-02 22:58:11 +0800

    Update sendMessage.js

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -16,43 +16,31 @@ async function typingIndicator(senderId, pageAccessToken) {
 
 function sendMessage(senderId, message, pageAccessToken) {
   if (!message || (!message.text && !message.attachment)) {
-    console.error('Error: Message must provide valid text or attachment.');
+    console.error("Message must contain 'text' or 'attachment'.");
     return;
-  }
-
-  const payload = {
-    recipient: { id: senderId },
-    message: {}
-  };
-
-  if (message.text) {
-    payload.message.text = message.text;
-  }
-
-  if (message.attachment) {
-    payload.message.attachment = message.attachment;
-  }
-
-  if (message.quick_replies) {
-    payload.message.quick_replies = message.quick_replies;
   }
 
   typingIndicator(senderId, pageAccessToken);
 
-  request({
-    url: 'https://graph.facebook.com/v13.0/me/messages',
-    qs: { access_token: pageAccessToken },
-    method: 'POST',
-    json: payload,
-  }, (error, response, body) => {
-    if (error) {
-      console.error('Error sending message:', error);
-    } else if (response.body.error) {
-      console.error('Error response:', response.body.error);
-    } else {
-      console.log('Message sent successfully:', body);
+  const requestData = {
+    recipient: { id: senderId },
+    message,
+  };
+
+  request.post(
+    {
+      url: `https://graph.facebook.com/v13.0/me/messages`,
+      qs: { access_token: pageAccessToken },
+      json: requestData,
+    },
+    (error, response, body) => {
+      if (error) {
+        console.error('Error sending message:', error);
+      } else {
+        console.log('Message sent successfully:', body);
+      }
     }
-  });
+  );
 }
 
 module.exports = { sendMessage };
