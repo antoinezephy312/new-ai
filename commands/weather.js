c5d21b0293d41f77d669ee8a9a5f932f208918db
antoinezephy312 <antoinezephyr312@gmail.com>
2024-12-14 00:13:21 +0800

    Create weather.js

---
+++ commands/weather.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+const fs = require('fs');
+
+const token = fs.readFileSync('token.txt', 'utf8');
+
+module.exports = {
+  name: 'weather',
+  description: 'Get the current weather of a city',
+  author: 'Clarence',
+  usage:'weather [city]',
+
+  async execute(senderId, args) {
+    const pageAccessToken = token;
+    const city = args.join(' ').trim();
+
+    // Vérifier si une ville a été fournie
+    if (!city) {
+      await sendMessage(senderId, { text: 'Please enter the name of a city to get the weather forecast. Ex: Valencia City weather' }, pageAccessToken);
+      return;
+    }
+
+    try {
+      // Faire une requête à l'API de wttr.in
+      const response = await axios.get(`https://wttr.in/${encodeURIComponent(city)}?format=%C+%t+%h+%w&m`);
+
+      const weatherData = response.data; // Ex: "Partly cloudy +27°C 64% Humidity ↙ 15 km/h"
+      const formattedMessage = `🌤️ Weather in ${city} :\n\n${weatherData}`;
+
+      // Envoyer la réponse à l'utilisateur
+      await sendMessage(senderId, { text: formattedMessage }, pageAccessToken);
+
+    } catch (error) {
+      console.error('Error searching for weather:', error.message);
+      await sendMessage(senderId, { text: 'Unable to retrieve weather forecast. Check the city name or try again later.' }, pageAccessToken);
+    }
+  }
+};
