d6940488cdedb5f8bc4e3827247f7070befd7ab6
antoinezephy312 <antoinezephyr312@gmail.com>
2024-12-14 11:06:37 +0800

    Create remini.js

---
+++ commands/remini.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'remini',
+  description: 'Enhance Photo by replying your image.',
+  author: 'Clarence Mangigo',
+
+  async execute(bot, args, authToken, event) {
+    if (!event?.sender?.id) {
+      console.error('Invalid event object: Missing sender ID.');
+      sendMessage(bot, { text: 'Error: Missing sender ID.' }, authToken);
+      return;
+    }
+
+    const senderId = event.sender.id;
+    try {
+      const imageUrl = await extractImageUrl(event, authToken);
+
+      if (!imageUrl) {
+        return sendMessage(bot, {
+          text: `𝗣𝗹𝗲𝗮𝘀𝗲 𝘀𝗲𝗻𝗱 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝗳𝗶𝗿𝘀𝘁 𝗼𝗿 𝗿𝗲𝗽𝗹𝘆 𝘁𝗼 𝗮𝗻 𝗶𝗺𝗮𝗴𝗲 𝘄𝗶𝘁𝗵 "𝗿𝗲𝗺𝗶𝗻𝗶" 𝘁𝗼 𝗿𝗲𝗺𝗼𝘃𝗲 𝗶𝘁𝘀 𝗯𝗮𝗰𝗸𝗴𝗿𝗼𝘂𝗻𝗱.`
+        }, authToken);
+      }
+
+      await sendMessage(bot, { text: '⌛ 𝗘𝗻𝗵𝗮𝗻𝗰𝗶𝗻𝗴 𝗜𝗺𝗮𝗴𝗲, 𝗽𝗹𝗲𝗮𝘀𝗲 𝘄𝗮𝗶𝘁...' }, authToken);
+
+      const removeBgUrl = `https://api.ryzendesu.vip/api/ai/remini?url=${encodeURIComponent(imageUrl)}`;
+
+      await sendMessage(bot, {
+        attachment: {
+          type: 'image',
+          payload: { url: removeBgUrl }
+        }
+      }, authToken);
+
+    } catch (error) {
+      console.error('Error removing background:', error);
+      sendMessage(bot, {
+        text: 'An error occurred while processing the image. Please try again later.'
+      }, authToken);
+    }
+  }
+};
+
+async function extractImageUrl(event, authToken) {
+  try {
+    if (event.message.reply_to?.mid) {
+      return await getRepliedImage(event.message.reply_to.mid, authToken);
+    } else if (event.message?.attachments?.[0]?.type === 'image') {
+      return event.message.attachments[0].payload.url;
+    }
+  } catch (error) {
+    console.error('Failed to extract image URL:', error);
+  }
+  return '';
+}
+
+async function getRepliedImage(mid, authToken) {
+  try {
+    const { data } = await axios.get(`https://graph.facebook.com/v21.0/${mid}/attachments`, {
+      params: { access_token: authToken }
+    });
+    return data?.data[0]?.image_data?.url || '';
+  } catch (error) {
+    throw new Error('Failed to retrieve replied image.');
+  }
+}
