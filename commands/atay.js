a9f35d702567bd48ab3a38708d53874646858891
antoinezephy312 <antoinezephyr312@gmail.com>
2025-01-18 22:18:12 +0800

    Update atay.js

--- commands/atay.js
+++ commands/atay.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { sendMessage } = require('../handles/message');
+const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: "belat",
