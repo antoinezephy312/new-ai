266c255519ad15dec3de59b7d9c69ad8fc8b7267
antoinezephy312 <antoinezephyr312@gmail.com>
2025-02-28 11:30:31 +0800

    Update vision.js

--- commands/vision.js
+++ commands/vision.js
@@ -36,7 +36,7 @@ module.exports = {
         sendLongMessage(bot, visionResponse, authToken);
       } else {
         // If no image, use GPT API
-        const apiUrl = "https://dataforge-api-production.up.railway.app/api/gemma2";
+        const apiUrl = "https://dataforge-api-production.up.railway.app/api/pixtral";
         const response = await axios.get(apiUrl, {
           params: {
             prompt: finalPrompt,
